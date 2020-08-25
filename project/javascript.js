4a524fa392989e78827984aa3e20e2fe5f465614
Catrina Nicolae-Andrei <48060866+CatrinaAndrei@users.noreply.github.com>
2020-08-25 20:45:13 +0300

    hotfix slider (sort of),navbar

--- project/javascript.js
+++ project/javascript.js
@@ -1,53 +1,60 @@
-const dropdown = document.getElementById("dropdown");
-const dropdownList = document.getElementById("dropdown__list");
-const droplink = document.getElementById("nav__link--dropdown");
-const contact = document.getElementById("nav__item--contact");
-const navbar = document.getElementById("navbar");
+const dropdown = document.querySelector(".dropdown");
+const dropdownList = document.querySelector(".dropdown__list");
+const dropdownBefore = window.getComputedStyle(dropdown,"::before");
+const droplink = document.querySelector(".nav__link--dropdown");
+const contact = document.querySelector(".nav__item--contact");
+const navbar = document.querySelector(".navbar");
 const burgerBtn = document.querySelector(".navbar__hamburger");
-const hero = document.getElementById("hero");
-const sliderBtnActive = document.querySelectorAll(".hero__slider-btn");
+const hero = document.querySelector(".hero");
+const sliderBtnActive = document.querySelectorAll(".hero__sliderBtn");
 
 
 let menuOpen = false;
+let dropdownOpen = false;
 
 burgerBtn.addEventListener("click",()=>{
 	if (!menuOpen){
-		burgerBtn.classList.add("open");
-		navbar.style.visibility = "visible";
-        navbar.style.height= "815px";
+		burgerBtn.classList.remove("closed");
+		navbar.classList.remove("closed");
+		burgerBtn.classList.toggle("open");
+		navbar.classList.toggle("open");
 		menuOpen = true;
 	}
 	else {
-		burgerBtn.classList.remove('open');
-		navbar.style.visibility = "hidden";
-		navbar.style.height = "0px";
+		burgerBtn.classList.remove("open");
+		navbar.classList.remove("open");
+		burgerBtn.classList.toggle("closed");
+		navbar.classList.toggle("closed");
 		menuOpen = false;
 	}
 }
 );
 
 
-droplink.onclick = function(){
+droplink.addEventListener("click",()=>{
 
- 	if(dropdown.style.visibility == "hidden"){
-		dropdown.style.visibility = "visible";
-		dropdown.style.height = "186px";
-		dropdownList.style.visibility = "visible";
-		if (window.innerWidth <= 1152){
-			contact.style.paddingTop = "160px";
-		}
+ 	if(!dropdownOpen){
+ 		dropdown.classList.remove("closed");
+ 		dropdownList.classList.remove("closed");
+ 		contact.classList.remove("closed");
+		dropdown.classList.toggle("open");
+		dropdownList.classList.toggle("open");
+		contact.classList.toggle("open");
+		dropdownOpen = true;
 	}	
 	else
-	{
-		dropdown.style.visibility = "hidden";
-		dropdown.style.height = "0px";
-		dropdownList.style.visibility = "hidden";	
-		contact.style.paddingTop = "0px";    
+	{   dropdown.classList.remove("open");
+ 		dropdownList.classList.remove("open");
+ 		contact.classList.remove("open");
+		dropdown.classList.toggle("closed");
+		dropdownList.classList.toggle("closed");	
+		contact.classList.toggle("closed");
+		dropdownOpen = false;    
 	}
 }
+);
 
-
-var sliders = Array.from(sliderBtnActive);
+/*
 var images = [];
 
 
@@ -56,7 +63,6 @@ images[1] = "radial-gradient(58.3% 58.3% at 75.33% 44.51%, rgba(0, 0, 0, 0.0001)
 images[2] = "radial-gradient(58.3% 58.3% at 75.33% 44.51%, rgba(0, 0, 0, 0.0001) 0%, rgba(5, 4, 2, 0.6) 100%),url(about-cliff.jpg)";
 
 
-for (i = 0; i < sliders.length; i++){
  	sliderBtnActive[0].addEventListener("click",()=>{ 
 		hero.style.backgroundImage = images[0];
 	});
@@ -65,9 +71,35 @@ for (i = 0; i < sliders.length; i++){
 	});
 	sliderBtnActive[2].addEventListener("click",()=>{ 
 		hero.style.backgroundImage = images[2];
+	});*/
+
+
+	const slideArray = [];
+	for (let i = 0; i < document.querySelectorAll('.hero__slider div').length; i++) {
+		slideArray.push(document.querySelectorAll('.hero__slider div')[i].dataset.background);
+	}
+	
+	let isActive = false;
+	let currentSlideIndex = 0;
+
+	for (let i = 0; i < sliderBtnActive.length; i++)
+	sliderBtnActive[i].addEventListener("click",()=>{
+		
+	    if (isActive) {
+			isActive = false;
+			sliderBtnActive[i].classList.remove("active");
+  		} 	
+ 		 else {
+			  isActive = true;
+			sliderBtnActive[i].classList.add("active");
+			currentSlideIndex++;
+ 		} 
+		if (currentSlideIndex >= slideArray.length) {
+			currentSlideIndex = 0;
+		}
+	
+		document.querySelector('.hero').style.backgroundImage = 'radial-gradient(58.3% 58.3% at 75.33% 44.51%, rgba(0, 0, 0, 0.0001) 0%, rgba(5, 4, 2, 0.6) 100%),url("' + slideArray[currentSlideIndex] + '")';
+	    
 	});
-}
-
-
-
-
+	
+		
